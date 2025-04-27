debc33512bf77ad4fe3c7355b3fdabc1ab8ce237
vineyard77 <ailairizsa@gmail.com>
2025-04-27 15:15:03 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
-// React entry point
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-function App() {
-  return <h1>Harvest Projection Platform - Frontend</h1>;
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
 import React, { useEffect, useState } from 'react';
 
 function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("https://harvest-backend-ocea.onrender.com") //
+    fetch("https://harvest-backend-ocea.onrender.com/")  //
       .then(response => response.json())
       .then(data => setMessage(data.message))
       .catch(error => console.error('Error fetching data:', error));
