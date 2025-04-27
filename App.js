b378a7012e99d0df64ae3f15ce8e01a9f498c766
vineyard77 <ailairizsa@gmail.com>
2025-04-27 14:26:00 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+// React entry point
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return <h1>Harvest Projection Platform - Frontend</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ README.md
@@ -0,0 +1,2 @@
+# Frontend Repository
+This is the frontend React setup for Harvest Projection Platform.
